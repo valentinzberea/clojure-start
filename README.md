319db2887b626325538b6f725ff385772764ecfd
Valentin Zberea <valentin.zberea@gmail.com>
2015-05-24 00:05:22 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# clojure-start
+
+Some clojure experiments
+
+## Installation
+
+```zsh
+lein
+```
+
+## Usage
+
+To run the clojure program:
+
+```zsh
+    $ lein run
+```
+
+To see the clojure script playground:
+
+```shell
+    $ lein cljsbuild auto
+```
+
+```shell
+    $ cd target/resources/public && python -m SimpleHTTPServer
+```
+
+
+### Bugs
+
+...
---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject clojure-start "0.1.0-SNAPSHOT"
+  :description "Clojure experiments"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+                 [org.clojure/clojurescript "0.0-3269"]]
+  :main ^:skip-aot clojure-start.core
+  :source-paths ["src/clj"]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}}
+  :plugins [[lein-cljsbuild "1.0.6"]]
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src/cljs"]
+        :compiler {
+          :output-to "target/resources/public/main.js"
+          :optimizations :whitespace
+          :pretty-print true}}]})
