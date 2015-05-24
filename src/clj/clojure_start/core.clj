fac8ff90ff136b8176b248cf384fe681b63bd9db
Valentin Zberea <valentin.zberea@gmail.com>
2015-05-24 02:59:37 +0100

    Add a reduce experiment in core.clj
    Try later to make a test instead

--- src/clj/clojure_start/core.clj
+++ src/clj/clojure_start/core.clj
@@ -1,7 +1,17 @@
 (ns clojure-start.core
   (:gen-class))
 
+(defn say-hello [name]
+  (println "Hello" name))
+
+(defn show-reduce [arr]
+  (let [result (reduce + arr)]
+    (println result)))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  [(say-hello "Valentin")
+   (show-reduce [1 5 3])
+  ])
+
